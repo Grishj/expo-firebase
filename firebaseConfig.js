d88ae4ecff6fce89de1d937a72085e0dd266097a
Grish <joshigrish058@gmail.com>
2025-08-08 11:36:28 +0545

    structured code

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,19 +1,23 @@
-// firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_APP_ID,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+} from "react-native-dotenv";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqY_pTpX-VuFPZ6-zZmu3NBfZljLZpSpg",
-  authDomain: "expo-firebase-e18e6.firebaseapp.com",
-  projectId: "expo-firebase-e18e6",
-  storageBucket: "expo-firebase-e18e6.firebasestorage.app",
-  messagingSenderId: "429267301029",
-  appId: "1:429267301029:web:d5c1de9266636d88bf534c",
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
 export default app;
